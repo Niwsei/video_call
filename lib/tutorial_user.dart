3c6496b2669738205dea964f1c05077c6d263ff3
Niwsei <loveonelove2004@gmail.com>
2025-10-15 10:14:58 +0700

    eiei

--- lib/tutorial_user.dart
+++ lib/tutorial_user.dart
@@ -15,7 +15,7 @@ class TutorialUser {
           image:
               'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600',
         ),
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjhkYTU2MTBlODk5NDkxNmYwMTM2NGI4IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzU5NjUwMTMxLCJleHAiOjE3NTk2NTM3MzF9.JF3Hh62cftlQBrQYVHlXfQCk2E0kUfOOrPrtrkaeegs', // ใส่ STREAM USER TOKEN ของ userId นี้
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjhkYTU2MTBlODk5NDkxNmYwMTM2NGI4IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NzIwMCwiaWF0IjoxNzYwNDk0NzMwLCJleHAiOjE3NjA1MDE5MzB9.4yieYU-A6upanMzauFqI6D_LguxrvJL-FenmDrH41wM', // ใส่ STREAM USER TOKEN ของ userId นี้
       );
 
   // TODO: ใส่ Stream User Token จริงของ user2 ที่นี่
@@ -26,7 +26,7 @@ class TutorialUser {
           image:
               'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600',
         ),
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjhhZDFiYWJkMDNjNDRlZjk0M2JiNmJiIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzU5NjUwMDg4LCJleHAiOjE3NTk2NTM2ODh9.QC1AKQNxcxiM9ndpJMaIHpSRnIXwb2WbGGb_UPjZJM4',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjhhZDFiYWJkMDNjNDRlZjk0M2JiNmJiIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NzIwMCwiaWF0IjoxNzYwNDk2NzYxLCJleHAiOjE3NjA1MDM5NjF9.QKpDcVGNMWo2qaXub5iRpmejHCJO62F9m9c19LwEIqU',
       );
 
 
